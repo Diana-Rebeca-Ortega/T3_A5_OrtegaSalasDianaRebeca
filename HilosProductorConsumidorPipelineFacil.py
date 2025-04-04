55eb4ddfa04214b29f92fa5e04bdba0ab3f863fa
Diana Rebeca Ortega <diana.os@tecjerez>
2025-04-03 23:27:38 -0600

    8º commit: Se hizo el codigo mas sensicillo quitandole todas las declaraciones de registro

--- HilosProductorConsumidorPipelineFacil.py
+++ HilosProductorConsumidorPipelineFacil.py
@@ -1,11 +1,8 @@
-import random
 import logging
 import threading
 import time
 import concurrent.futures
-import queue
-
-SENTINEL = object()
+import random
 
 class Pipeline:
     """
@@ -18,49 +15,47 @@ class Pipeline:
         self.consumer_lock.acquire()
 
     def get_message(self, name):
-        logging.debug("%s:A punto de adquirir getlock", name)
         self.consumer_lock.acquire()
-        logging.debug("%s:tener bloqueo", name)
         message = self.message
-        logging.debug("%s:A punto de liberar el bloqueo", name)
         self.producer_lock.release()
-        logging.debug("%s:bloqueo liberado", name)
         return message
 
     def set_message(self, message, name):
-        logging.debug("%s:a punto de adquirir setlock", name)
         self.producer_lock.acquire()
-        logging.debug("%s:tener setlock", name)
         self.message = message
-        logging.debug("%s:A punto de liberar getlock", name)
         self.consumer_lock.release()
-        logging.debug("%s:obtener bloqueo liberado", name)
+        
+
+
+SENTINEL = object()
 
 def producer(pipeline):
-    """Imagina que recibimos un mensaje de la red."""
+    """Pretend we're getting a message from the network."""
     for index in range(10):
         message = random.randint(1, 101)
-        logging.info("El productor recibió un mensaje: %s", message)
+        logging.info("Producer got message: %s", message)
         pipeline.set_message(message, "Producer")
-    # Envíe un mensaje centinela para avisarle al consumidor que hemos terminado
-    pipeline.set_message(SENTINEL, "Producer")
 
+    # Send a sentinel message to tell consumer we're done
+    pipeline.set_message(SENTINEL, "Producer")        
+        
+        
 def consumer(pipeline):
     """Pretend we're saving a number in the database."""
-    while True:
+    message = 0
+    while message is not SENTINEL:
         message = pipeline.get_message("Consumer")
-        if message is SENTINEL:
-            break
-        logging.info("Consumer storing message: %s", message)
-
+        if message is not SENTINEL:
+            logging.info("Consumer storing message: %s", message)        
+        
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-    logging.getLogger().setLevel(logging.DEBUG)
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    # logging.getLogger().setLevel(logging.DEBUG)
+
     pipeline = Pipeline()
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        executor.submit(consumer, pipeline)
         executor.submit(producer, pipeline)
-        
-        
-        
+        executor.submit(consumer, pipeline)        
+    
